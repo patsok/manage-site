d9a943fca3a54d8571693d05da957d18a5481aae
Falco <patryk.sokol@gmail.com>
2022-08-17 14:29:26 +0200

    add testimonials design

---
+++ src/swiper.js
@@ -0,0 +1,58 @@
+import 'swiper/scss';
+import 'swiper/scss/autoplay';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+
+Swiper.use([Navigation, Pagination, Autoplay]);
+
+// var swiper = new Swiper(".swiper", {
+//     loop: true,
+//     // speed: 5000,
+//     // autoplay: {
+//     //   delay: 1000,
+//     // },
+//     centeredSlides: false,
+//     initialSlide: -1,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+
+//     slidesPerView: 3,
+//     spaceBetween: 500,
+//     // breakpoints: {
+//     //   768: {
+//     //     slidesPerView: 1,
+//     //     spaceBetween: 300,
+
+//     //   },
+//     //   1024: {
+//     //     slidesPerView: 3,
+//     //     spaceBetween: 300,
+//     //   },
+//     // },
+// });
+
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+    speed: 1000,
+    autoplay: {
+      delay: 3000,
+    },
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+      clickable: true,
+  },
+  breakpoints: {
+    1400: {
+       slidesPerView: 2,
+       spaceBetween: -250,
+    },
+  },
+});
