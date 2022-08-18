960eb91ab9a56352d9dc732c09d86d93b35eab3b
Falco <patryk.sokol@gmail.com>
2022-08-18 21:57:46 +0200

    final changes

--- src/index.js
+++ src/index.js
@@ -4,25 +4,14 @@ const hamburgerMenu = document.querySelector(".hamburger");
 const closeButton = document.querySelector(".close-button");
 const mobileNav = document.querySelector('.nav-items');
 const background = document.querySelector('.background')
-const nav = document.querySelector('.nav');
 
-hamburgerMenu.addEventListener('click',showNav);
-closeButton.addEventListener('click',closeNav);
+hamburgerMenu.addEventListener('click',toggleNav);
+closeButton.addEventListener('click',toggleNav);
 
-function showNav(){
-    hamburgerMenu.classList.add('mobile-hidden');
-    mobileNav.classList.add('nav-mobile');
-    closeButton.classList.remove('mobile-hidden');
-    background.classList.remove('hidden');
-    background.classList.add('gradient');
-    nav.setAttribute('style','padding-bottom:54px');
-}
-
-function closeNav(){
-    hamburgerMenu.classList.remove('mobile-hidden');
-    mobileNav.classList.remove('nav-mobile');
-    closeButton.classList.add('mobile-hidden');
-    background.classList.remove('gradient');
-    background.classList.add('hidden');
-    nav.setAttribute('style','position:static');
+function toggleNav(){
+    hamburgerMenu.classList.toggle('mobile-hidden');
+    mobileNav.classList.toggle('nav-mobile');
+    closeButton.classList.toggle('mobile-hidden');
+    background.classList.toggle('gradient');
+    background.classList.toggle('hidden');
 }
