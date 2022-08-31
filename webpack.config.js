c73b083b7608e6d9c8cee3e032659fcda541aac7
Falco <patryk.sokol@gmail.com>
2022-08-31 22:35:41 +0200

    php try

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,60 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const fileContent = require("php!./file.php");
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    entry: {
-      index: './src/index.js',
-      nav: './src/_sections/nav/nav.js',
-      header: './src/_sections/header/header.js',
-      testimonials: './src/_sections/testimonials/testimonials.js',
-      footer: './src/_sections/footer/footer.js',
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        static: './dist',
-    },
-    optimization: {
-      runtimeChunk: 'single'
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    index: './src/index.js',
+    nav: './src/_sections/nav/nav.js',
+    header: './src/_sections/header/header.js',
+    testimonials: './src/_sections/testimonials/testimonials.js',
+    footer: './src/_sections/footer/footer.js',
   },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.html"
-      })
-    ],
-    module: {
-        rules: [{
-            test: /\.s[ac]ss$/i,
-            use: ["style-loader", "css-loader", "sass-loader"]
-            },
-            {
-              test: /\.css$/i,
-              use: ['style-loader', 'css-loader'],
-            },
-            {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-            },
-            {
-              test: /\.(woff|woff2|eot|ttf|otf)$/i,
-              type: 'asset/resource',
-            },
-            {
-              test: /\.html$/i,
-              loader: 'html-loader',
-            }
-          ]
-    }
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.php"
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.s[ac]ss$/i,
+      use: ["style-loader", "css-loader", "sass-loader"]
+    },
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource', 
+      generator: {
+        filename: 'images/[name].[ext]'
+      }
+    },
+    {
+      test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+      type: 'asset',
+      generator: {
+        filename: 'fonts/[name].[ext]'
+      }
+    },
+      {
+        test: /\.html|php$/i,
+        loader: 'html-loader',
+      },
+
+    ]
+  }
 };
