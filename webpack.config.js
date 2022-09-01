3461dcb974e05f621f6e9d496d3788c17cf68976
Falco <patryk.sokol@gmail.com>
2022-09-01 11:03:31 +0200

    fix animations

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
+  mode: 'production',
+
+    // mode: 'development',
+    // devtool: 'inline-source-map',
     entry: {
       index: './src/index.js',
       nav: './src/_sections/nav/nav.js',
@@ -15,9 +17,9 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
-    devServer: {
-        static: './dist',
-    },
+    // devServer: {
+    //     static: './dist',
+    // },
     optimization: {
       runtimeChunk: 'single'
   },
@@ -36,12 +38,18 @@ module.exports = {
               use: ['style-loader', 'css-loader'],
             },
             {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
+              test: /\.(png|svg|jpg|jpeg|gif)$/i,
+              type: 'asset/resource', 
+              generator: {
+                filename: 'images/[name].[ext]'
+              }
             },
             {
-              test: /\.(woff|woff2|eot|ttf|otf)$/i,
-              type: 'asset/resource',
+              test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+              type: 'asset',
+              generator: {
+                filename: 'fonts/[name].[ext]'
+              }
             },
             {
               test: /\.html$/i,
